7fadaaea0f7a9d320936a76aa5ad7bce0580677f
Karen V <karenvo@usc.edu>
2019-02-26 11:03:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+      # a <=> b
+end
 
+def sort_array_desc(array)
+
+end
+
+def sort_array_char_count(array)
+
+end
