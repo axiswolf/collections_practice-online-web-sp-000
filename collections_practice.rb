4f3d4d62c40a25d6399b7a0e55b2bd7ea659b4f6
Karen V <karenvo@usc.edu>
2019-02-26 11:06:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,12 @@
 def sort_array_asc(array)
-  array.each do |a, b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-      # a <=> b
+  array.sort do |a, b|
+    #if a == b
+      #0
+    #elsif a < b
+      #-1
+    #elsif a > b
+      #1
+      a <=> b
     end
 end
 
